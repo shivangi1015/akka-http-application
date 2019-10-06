46016f94c1bd6c4b8088cf5d766f17c733f4cb56
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-10-06 23:05:49 +0530

    Added macwire DI

--- build.sbt
+++ build.sbt
@@ -7,4 +7,5 @@ scalaVersion := "2.12.6"
 libraryDependencies ++= Seq("com.typesafe.akka" %% "akka-actor" % "2.5.13",
   "com.typesafe.akka" %% "akka-stream" % "2.5.13",
   "com.typesafe.akka" %% "akka-http" % "10.1.3",
-  "org.scalatest" %% "scalatest" % "3.0.8-RC5" % Test)
+  "org.scalatest" %% "scalatest" % "3.0.8-RC5" % Test,
+  "com.softwaremill.macwire" %% "macros" % "2.3.0" % "provided")
--- src/main/scala/server/Boot.scala
+++ src/main/scala/server/Boot.scala
@@ -4,6 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import server.controller.RouteHandler
+import com.softwaremill.macwire._
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
@@ -14,7 +15,7 @@ object Boot extends App {
   implicit val executor: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  val router = new RouteHandler
+  lazy val router = wire[RouteHandler]
   val binding = Http()
     .bindAndHandle(router.route, ConfigReader.host, ConfigReader.port)
   binding.onComplete {
