8076203da0830674617226b0d3ee33e67b8e8aec
shivangi Gupta <shivangi.gupta@knoldus.in>
2019-10-14 00:38:56 +0530

    configured h2 database

--- src/main/scala/server/repository/UserTable.scala
+++ src/main/scala/server/repository/UserTable.scala
@@ -1,10 +1,26 @@
 package server.repository
 
 // Import the Slick interface for H2:
-import slick.lifted.Tag
-import slick.model.Table
+import slick.jdbc.H2Profile.api._
 
-final class UserTable(tag: Tag) extends Table[User](tag, "user") {
+trait UserTable {
 
-  def id = columns[Int]("id", O.PrimaryKey, O.AutoInc)
+  // Create an in-memory H2 database;
+  val db = Database.forConfig("chapter01")
+
+  class UserTableMapping(tag: Tag) extends Table[User](tag, "user") {
+
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def fname = column[String]("fname")
+    def lname = column[String]("lname")
+    def age = column[Int]("age")
+    def gender = column[String]("gender")
+    def state = column[String]("state")
+    def country = column[String]("country")
+
+    def * = (id, fname, lname, age, gender, state, country).mapTo[User]
+  }
+
+  lazy val messages = TableQuery[UserTableMapping]
+
 }
